1edee955b9d032c13a8e9494af4b79eebda92d2c
Marek Kolodziej <mkolod@gmail.com>
2013-07-12 13:06:33 -0400

    Merge conflict fix

--- src/com/visiblemeasures/ma/Master.scala
+++ src/com/visiblemeasures/ma/Master.scala
@@ -19,15 +19,7 @@ class Master(window: Int, stocks: List[StockInit]) extends Actor {
  
     case Start => {
       
-<<<<<<< HEAD
-<<<<<<< HEAD
         stocks.foreach(i => createActor(window, i.initPrice, i.sym.name) ! i.sym)
-=======
-        stocks.foreach(i => createActor(window, i.initPrice, i.sym.name) ! i)
->>>>>>> 7757857c938690ee320dda58c87da0f8ab1b6abb
-=======
-        stocks.foreach(i => createActor(window, i.initPrice, i.sym.name) ! i)
->>>>>>> 7757857c938690ee320dda58c87da0f8ab1b6abb
     }
       
   }
